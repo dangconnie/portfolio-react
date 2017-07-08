020e01b8c66af2c894e92759562ccfac2384b52b
Connie Dang <conniedang576@gmail.com>
2017-07-07 22:28:08 -0400

    about section

--- src/About/about.js
+++ src/About/about.js
@@ -8,7 +8,14 @@ class About extends Component{
 		return(
 			<div className="about">
 				<h1>about me section </h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquet sem ut massa aliquet molestie. Suspendisse eu nunc sem. Donec at nisl mauris. Morbi feugiat ligula id nisl lobortis, et fermentum velit faucibus. Etiam pretium elementum metus in consequat. Curabitur interdum lectus ut velit consequat porta. In porta lorem non quam tristique, vel pharetra massa dapibus. Donec nec lectus venenatis, mollis arcu sed, interdum quam. Curabitur gravida molestie ultrices. Donec dictum nibh eu neque aliquet, at volutpat massa malesuada. Cras vel nunc tristique elit vehicula fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut molestie turpis ex, vel scelerisque augue consectetur sit amet. Fusce iaculis ac ipsum eu malesuada. Etiam sodales ut diam in accumsan.</p>
+				<div className="col-sm-6 backgroundInfo">	
+					<h3>Background</h3>
+					<p></p>
+				</div>
+				<div className="col-sm-6 webDevInfo">
+					<h3>Web Development</h3>
+					<p>I'm an aspiring developer</p>
+				</div>
 			</div>
 		)
 	}
